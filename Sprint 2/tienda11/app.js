9ad00a5a0b0a4fca3d0e43d666699f3f52ce30b6
SantiagoChappa <santiagochappa@gmail.com>
2021-07-26 21:34:15 -0300

    Diseño-Maqueta del home y actualizacion de app.js

--- Sprint 2/tienda11/app.js
+++ Sprint 2/tienda11/app.js
@@ -3,6 +3,8 @@ const path= require('path')
 const app= express()
 const port=3000
 
+app.use(express.static('public'))
+
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname, './views/index.html'))
 })
@@ -23,8 +25,9 @@ app.get('/registro', (req,res)=>{
     res.sendFile(path.join(__dirname, './views/register.html'))
 })
 
-app.use(express.static('public'))
+
 
 app.listen(port, ()=>{
     console.log('Servidor funcionando en http://localhost:'+port);
 })
+
